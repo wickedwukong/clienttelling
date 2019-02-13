b0c2bb208806b7d83423874105c1c698040a5f20
Xuemin Guan <Xuemin Guan>
2019-02-13 21:43:06 +0000

    display email, appointment and message icons under customer name in rec page

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,22 @@ class ClienttellingApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-   _getCustomers () => <Customer>[
-    Customer("Joanna", "Biggar", [
-      Recommendation("Gina Bacconi Brielle Dress, Pink", "1.jpeg"),
-      Recommendation(
-          "Phase Eight Emanuella Floral Printed Dress, Oyster", "2.jpeg")
-    ]),
-    Customer("Esther", "Jackson",
-        [Recommendation("Modern Rarity Ruffle Front Dress, Pink", "3.jpeg")]),
-    Customer("Daisy", "Woodward", []),
-    Customer("Gabrielle", "John", []),
-    Customer("Amy", "Ixer", []),
-    Customer("Angela", "Worley", []),
-    Customer("Rohina", "Adams", []),
-    Customer("Lucy", "Tamley", [])
-  ];
+  _getCustomers() => <Customer>[
+        Customer("Joanna", "Biggar", [
+          Recommendation("Gina Bacconi Brielle Dress, Pink", "1.jpeg"),
+          Recommendation(
+              "Phase Eight Emanuella Floral Printed Dress, Oyster", "2.jpeg")
+        ]),
+        Customer("Esther", "Jackson", [
+          Recommendation("Modern Rarity Ruffle Front Dress, Pink", "3.jpeg")
+        ]),
+        Customer("Daisy", "Woodward", []),
+        Customer("Gabrielle", "John", []),
+        Customer("Amy", "Ixer", []),
+        Customer("Angela", "Worley", []),
+        Customer("Rohina", "Adams", []),
+        Customer("Lucy", "Tamley", [])
+      ];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildCustomers() {
@@ -71,7 +72,7 @@ class RandomWordsState extends State<RandomWords> {
         new MaterialPageRoute<void>(
           builder: (BuildContext context) {
             final Iterable<ListTile> tiles = customer.recs.map(
-                  (Recommendation rec) {
+              (Recommendation rec) {
                 return new ListTile(
                     title: new Text(
                       rec.title,
@@ -91,7 +92,17 @@ class RandomWordsState extends State<RandomWords> {
 
             return new Scaffold(
               appBar: new AppBar(
-                title: Text(customer.fullName()),
+                title: Column(children: [
+                  Text(customer.fullName()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      new Icon(Icons.email),
+                      new Icon(Icons.alarm),
+                      new Icon(Icons.message),
+                    ],
+                  ),
+                ]),
               ),
               body: new ListView(children: divided),
             ); // ... to here.
